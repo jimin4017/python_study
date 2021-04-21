894090e0a8f1abf85dd9978a511167de6541614c
jimin4017 <70991017+jimin4017@users.noreply.github.com>
2021-04-21 23:56:31 +0900

    Update visual.py

--- visual.py
+++ visual.py
@@ -130,3 +130,32 @@ for movie in movies:
 
 ### 깃허브 오류로 업데이트가 안되서 일시적인 업데이트
 
+
+import time
+ 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option('prefs',{'download.default_directory':r'C:\Users\정지민\Desktop\python_project\python_project'})
+
+
+browser = webdriver.Chrome(options=chrome_options)
+
+browser.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_a_download")
+
+browser.switch_to.frame('iframeResult')
+
+
+
+elem = browser.find_element_by_xpath('/html/body/p[2]/a')
+elem.click()
+
+print(browser.title)
+
+time.sleep(5)
+
+browser.quit( )
+
+## 깃허브 오류 
